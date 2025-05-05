8eebb35f49e3a48d7fea932e10a8b8a9bbba47eb
RafaelOliveira1503 <rafael_tf@outlook.com>
2025-05-05 16:52:15 -0300

    mudanças

---
+++ index.js
@@ -0,0 +1,47 @@
+const searchInput = document.getElementById('searchInput')
+
+const produtos = [
+    {
+        img: './assets/livro.jpg',
+        titulo: 'Ponto de Inflexão',
+        categoria: 'Desenvolvimento Pessoal',
+        preco: 69.99,
+        desconto: true
+    },
+    {
+        img: './assets/livro.jpg',
+        titulo: 'Ponto de O Poder do Aprendizado',
+        categoria: 'Desenvolvimento Pessoal',
+        preco: 69.99,
+        desconto: true
+    }, 
+    {
+        img: './assets/livro.jpg',
+        titulo: 'Mais esperto que o diabo',
+        categoria: 'Desenvolvimento Pessoal',
+        preco: 69.99,
+        desconto: true
+    }, 
+    {
+        img: './assets/livro.jpg',
+        titulo: 'O que se come no ceu?',
+        categoria: 'Desenvolvimento Pessoal',
+        preco: 69.99,
+        desconto: true
+    }, 
+    {
+        img: './assets/livro.jpg',
+        titulo: 'A psicologia financeira',
+        categoria: 'Desenvolvimento Pessoal',
+        preco: 69.99,
+        desconto: true
+    }, 
+    {
+        img: './assets/livro.jpg',
+        titulo: 'A meta',
+        categoria: 'Desenvolvimento Pessoal',
+        preco: 69.99,
+        desconto: true
+    },
+
+]
